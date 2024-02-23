86a6b143459758977efbbcdfe4d83995b68247b9
Chris Lozzi <clozzi12@gmail.com>
2024-02-23 10:39:58 -0500

    media details to Media

--- client/src/components/Media.js
+++ client/src/components/Media.js
@@ -8,21 +8,44 @@ function Media() {
     const { user } = useContext(UserContext)
     const { id } = useParams()
     const [media, setMedia] = useState({})
+    const [reviews, setReviews] = useState([])
 
     useEffect(() => {
         fetch(`/api/medias/${id}`)
         .then((r) => {
             if (r.status === 200) {
-                r.json().then((data) => setMedia(data))
+                r.json().then((data) => handleMedia(data))
             } else {
                 console.log('error retrieving data')
             }})
     }, [id])
 
-    
+    function handleMedia(media) {
+        setMedia(media)
+        setReviews(media.reviews)
+    }
+
+    function handleCreateReview(newReview) {
+        setReviews([...reviews, newReview])
+    }
 
     return (
-        <h1>{media.title}</h1>
+        <div className="media" key={media.id} >
+                    <img src={media.image_url} alt="media" width="100" height="100"/>
+                    <h3>{media.title}</h3>
+                    <h5>{media.media_type}</h5>
+                    <h5>Streaming on: {media.streaming_platform}</h5>
+                    {/* <CreateReview media={media} user={user} onCreateReview={handleCreateReview} /> */}
+                    <div>
+                        {reviews.map((review) => (
+                            <div key={review.id}>
+                            <p>Rating: {review.rating}</p>
+                            <p>Explanation:{review.comment}</p>
+                            <p>User: {review.user.username}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
     )
 }
 
