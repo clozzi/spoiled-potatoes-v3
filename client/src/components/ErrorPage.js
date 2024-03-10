8798e035dcda5f594c4569fa8fc0af1ead08d8af
Chris Lozzi <clozzi12@gmail.com>
2024-03-10 17:54:49 -0400

    fixed ErrorPage

--- client/src/components/ErrorPage.js
+++ client/src/components/ErrorPage.js
@@ -1,20 +1,12 @@
-import NavBar from "./NavBar";
-import { useRouteError } from "react-router-dom";
 
 
 function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
 
   return (
-    <>
-      <header>
-        <NavBar />
-      </header>
       <main>
         <h1>Whoops! Something went wrong!</h1>
+        <p>The page you are looking for does not exist.</p>
       </main>
-    </>
   );
 }
 
